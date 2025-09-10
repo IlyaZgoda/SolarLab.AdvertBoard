1e4b5557039997e8b6b793fb252e19826578f6a8
IlyaZgoda <zgoda-games@mail.ru>
2025-09-11 01:17:46 +0700

    add maybe type

---
+++ src/SharedKernel/SolarLab.AdvertBoard.SharedKernel/Maybe/Maybe.cs
@@ -0,0 +1,21 @@
+﻿namespace SolarLab.AdvertBoard.SharedKernel.Maybe
+{
+    public sealed record Maybe<T>(T? Value)
+    {
+        public bool HasValue => Value is not null;
+        public bool HasNoValue => !HasValue;
+
+        public T GetValue() => HasValue
+            ? Value!
+            : throw new InvalidOperationException("The value can not be accessed because it does not exist");
+
+        public static Maybe<T> None => new(default);
+        public static Maybe<T> From(T value) => new(value);
+
+        public static implicit operator Maybe<T>(T value) => From(value);
+        public static explicit operator T(Maybe<T> maybe) => maybe.GetValue();
+
+        public override string ToString() =>
+            HasValue ? $"Maybe({Value})" : "Maybe(None)";
+    }
+}
