f193ebce485622f9123cb88fca265ff7e1734bde
IlyaZgoda <zgoda-games@mail.ru>
2025-10-15 00:36:44 +0700

    add category tests

---
+++ tests/UnitTests/SolarLab.AdvertBoard.UnitTests/Domain/Categories/CategoryTests.cs
@@ -0,0 +1,198 @@
+﻿using FluentAssertions;
+using SolarLab.AdvertBoard.Domain.Categories;
+
+namespace SolarLab.AdvertBoard.UnitTests.Domain.Categories
+{
+    public class CategoryTests
+    {
+        private readonly CategoryTitle _title;
+
+        public CategoryTests()
+        {
+            _title = CategoryTitle.Create("Electronics").Value;
+        }
+
+        [Fact]
+        public void CreateRoot_Should_Create_Category_With_Correct_Properties()
+        {
+            // Act
+            var category = Category.CreateRoot(_title);
+
+            // Assert
+            category.Should().NotBeNull();
+            category.Id.Should().NotBeNull();
+            category.Title.Should().Be(_title);
+            category.ParentId.Should().BeNull();
+            category.Childrens.Should().BeEmpty();
+            category.CanHostAdverts.Should().BeTrue();
+        }
+
+        [Fact]
+        public void AddChild_Should_Create_Child_Category_With_Correct_Properties()
+        {
+            // Arrange
+            var parent = Category.CreateRoot(_title);
+            var childTitle = CategoryTitle.Create("Smartphones").Value;
+
+            // Act
+            var child = parent.AddChild(childTitle);
+
+            // Assert
+            child.Should().NotBeNull();
+            child.Id.Should().NotBeNull();
+            child.Title.Should().Be(childTitle);
+            child.ParentId.Should().Be(parent.Id);
+            child.Childrens.Should().BeEmpty();
+            child.CanHostAdverts.Should().BeTrue();
+        }
+
+        [Fact]
+        public void AddChild_Should_Add_Child_To_Parent_Childrens_Collection()
+        {
+            // Arrange
+            var parent = Category.CreateRoot(_title);
+            var childTitle = CategoryTitle.Create("Smartphones").Value;
+
+            // Act
+            var child = parent.AddChild(childTitle);
+
+            // Assert
+            parent.Childrens.Should().ContainSingle();
+            parent.Childrens.First().Should().Be(child);
+        }
+
+        [Fact]
+        public void CanHostAdverts_Should_Be_True_When_No_Children()
+        {
+            // Arrange
+            var category = Category.CreateRoot(_title);
+
+            // Act & Assert
+            category.CanHostAdverts.Should().BeTrue();
+        }
+
+        [Fact]
+        public void CanHostAdverts_Should_Be_False_When_Has_Children()
+        {
+            // Arrange
+            var parent = Category.CreateRoot(_title);
+            var childTitle = CategoryTitle.Create("Smartphones").Value;
+
+            // Act
+            parent.AddChild(childTitle);
+
+            // Assert
+            parent.CanHostAdverts.Should().BeFalse();
+        }
+
+        [Fact]
+        public void AddChild_Should_Create_Nested_Hierarchy()
+        {
+            // Arrange
+            var electronics = Category.CreateRoot(CategoryTitle.Create("Electronics").Value);
+            var smartphones = electronics.AddChild(CategoryTitle.Create("Smartphones").Value);
+            var iPhones = smartphones.AddChild(CategoryTitle.Create("iPhones").Value);
+
+            // Assert
+            electronics.ParentId.Should().BeNull();
+            electronics.Childrens.Should().ContainSingle().Which.Should().Be(smartphones);
+
+            smartphones.ParentId.Should().Be(electronics.Id);
+            smartphones.Childrens.Should().ContainSingle().Which.Should().Be(iPhones);
+
+            iPhones.ParentId.Should().Be(smartphones.Id);
+            iPhones.Childrens.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void CanHostAdverts_Should_Work_Correctly_In_Nested_Hierarchy()
+        {
+            // Arrange
+            var electronics = Category.CreateRoot(CategoryTitle.Create("Electronics").Value);
+            var smartphones = electronics.AddChild(CategoryTitle.Create("Smartphones").Value);
+            var iPhones = smartphones.AddChild(CategoryTitle.Create("iPhones").Value);
+
+            // Assert
+            electronics.CanHostAdverts.Should().BeFalse(); 
+            smartphones.CanHostAdverts.Should().BeFalse(); 
+            iPhones.CanHostAdverts.Should().BeTrue(); 
+        }
+
+        [Fact]
+        public void AddChild_Should_Allow_Multiple_Children_For_Same_Parent()
+        {
+            // Arrange
+            var electronics = Category.CreateRoot(CategoryTitle.Create("Electronics").Value);
+            var smartphones = CategoryTitle.Create("Smartphones").Value;
+            var laptops = CategoryTitle.Create("Laptops").Value;
+            var tablets = CategoryTitle.Create("Tablets").Value;
+
+            // Act
+            var child1 = electronics.AddChild(smartphones);
+            var child2 = electronics.AddChild(laptops);
+            var child3 = electronics.AddChild(tablets);
+
+            // Assert
+            electronics.Childrens.Should().HaveCount(3);
+            electronics.Childrens.Should().Contain(new[] { child1, child2, child3 });
+        }
+
+        [Fact]
+        public void AddChild_Should_Create_Independent_Children()
+        {
+            // Arrange
+            var parent1 = Category.CreateRoot(CategoryTitle.Create("Electronics").Value);
+            var parent2 = Category.CreateRoot(CategoryTitle.Create("Books").Value);
+            var childTitle = CategoryTitle.Create("Smartphones").Value;
+
+            // Act
+            var child1 = parent1.AddChild(childTitle);
+            var child2 = parent2.AddChild(childTitle);
+
+            // Assert
+            child1.Should().NotBe(child2);
+            child1.Id.Should().NotBe(child2.Id);
+            child1.ParentId.Should().Be(parent1.Id);
+            child2.ParentId.Should().Be(parent2.Id);
+        }
+
+        [Fact]
+        public void Childrens_Collection_Should_Be_Immutable_From_Outside()
+        {
+            // Arrange
+            var parent = Category.CreateRoot(_title);
+            var childTitle = CategoryTitle.Create("Smartphones").Value;
+            var child = parent.AddChild(childTitle);
+
+            // Act & Assert 
+            var childrens = parent.Childrens;
+
+            childrens.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void CreateRoot_Should_Work_With_Different_Titles()
+        {
+            // Test various category titles
+            var testTitles = new[]
+            {
+                CategoryTitle.Create("Electronics").Value,
+                CategoryTitle.Create("Home & Garden").Value,
+                CategoryTitle.Create("Real Estate").Value,
+                CategoryTitle.Create("Automotive").Value,
+                CategoryTitle.Create("Jobs").Value
+            };
+
+            foreach (var title in testTitles)
+            {
+                // Act
+                var category = Category.CreateRoot(title);
+
+                // Assert
+                category.Title.Should().Be(title);
+                category.ParentId.Should().BeNull();
+                category.Childrens.Should().BeEmpty();
+            }
+        }
+    }
+}
---
+++ tests/UnitTests/SolarLab.AdvertBoard.UnitTests/Domain/Categories/CategoryTitleTests.cs
@@ -0,0 +1,201 @@
+﻿using FluentAssertions;
+using SolarLab.AdvertBoard.Domain.Categories;
+using SolarLab.AdvertBoard.Domain.Errors;
+
+namespace SolarLab.AdvertBoard.UnitTests.Domain.Categories
+{
+    public class CategoryTitleTests
+    {
+        [Theory]
+        [InlineData("Electronics")]
+        [InlineData("Books")]
+        [InlineData("Real Estate")]
+        [InlineData("Automotive")]
+        [InlineData("Home & Garden")]
+        public void Create_Should_Succeed_When_Title_Valid(string validTitle)
+        {
+            // Act
+            var result = CategoryTitle.Create(validTitle);
+
+            // Assert
+            result.IsSuccess.Should().BeTrue();
+            result.Value.Value.Should().Be(validTitle);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("   ")]
+        [InlineData(null)]
+        public void Create_Should_Fail_When_Title_Empty(string emptyTitle)
+        {
+            // Act
+            var result = CategoryTitle.Create(emptyTitle);
+
+            // Assert
+            result.IsSuccess.Should().BeFalse();
+            result.IsFailure.Should().BeTrue();
+            result.Error.Should().Be(CategoryErrors.Title.Empty);
+        }
+
+        [Theory]
+        [InlineData("ab")] 
+        [InlineData("a")]  
+        public void Create_Should_Fail_When_Title_Too_Short(string shortTitle)
+        {
+            // Act
+            var result = CategoryTitle.Create(shortTitle);
+
+            // Assert
+            result.IsFailure.Should().BeTrue();
+            result.Error.Should().Be(CategoryErrors.Title.TooShort);
+        }
+
+        [Fact]
+        public void Create_Should_Fail_When_Title_Too_Long()
+        {
+            // Arrange
+            var longTitle = new string('a', CategoryTitle.MaxLength + 1);
+
+            // Act
+            var result = CategoryTitle.Create(longTitle);
+
+            // Assert
+            result.IsFailure.Should().BeTrue();
+            result.Error.Should().Be(CategoryErrors.Title.TooLong);
+        }
+
+        [Fact]
+        public void Create_Should_Fail_With_Correct_Error_For_Each_Validation()
+        {
+            // Test empty
+            var emptyResult = CategoryTitle.Create("");
+            emptyResult.IsFailure.Should().BeTrue();
+            emptyResult.Error.Should().Be(CategoryErrors.Title.Empty);
+
+            // Test too short
+            var shortResult = CategoryTitle.Create("ab");
+            shortResult.IsFailure.Should().BeTrue();
+            shortResult.Error.Should().Be(CategoryErrors.Title.TooShort);
+
+            // Test too long
+            var longTitle = new string('a', CategoryTitle.MaxLength + 1);
+            var longResult = CategoryTitle.Create(longTitle);
+            longResult.IsFailure.Should().BeTrue();
+            longResult.Error.Should().Be(CategoryErrors.Title.TooLong);
+        }
+
+        [Fact]
+        public void Create_Should_Create_Different_Instances_For_Same_Value()
+        {
+            // Arrange
+            var title = "Electronics";
+
+            // Act
+            var instance1 = CategoryTitle.Create(title).Value;
+            var instance2 = CategoryTitle.Create(title).Value;
+
+            // Assert
+            instance1.Should().NotBeSameAs(instance2);
+            instance1.Should().Be(instance2);
+        }
+
+        [Fact]
+        public void Create_Should_Handle_Min_Length_Title()
+        {
+            // Arrange
+            var minLengthTitle = new string('a', CategoryTitle.MinLength);
+
+            // Act
+            var result = CategoryTitle.Create(minLengthTitle);
+
+            // Assert
+            result.IsSuccess.Should().BeTrue();
+            result.Value.Value.Should().Be(minLengthTitle);
+        }
+
+        [Fact]
+        public void Create_Should_Handle_Max_Length_Title()
+        {
+            // Arrange
+            var maxLengthTitle = new string('a', CategoryTitle.MaxLength);
+
+            // Act
+            var result = CategoryTitle.Create(maxLengthTitle);
+
+            // Assert
+            result.IsSuccess.Should().BeTrue();
+            result.Value.Value.Should().Be(maxLengthTitle);
+        }
+
+        [Theory]
+        [InlineData("Home & Garden")]
+        [InlineData("Real Estate")]
+        [InlineData("Sport & Fitness")]
+        [InlineData("Beauty & Health")]
+        public void Create_Should_Succeed_When_Title_With_Special_Characters(string validTitle)
+        {
+            // Act
+            var result = CategoryTitle.Create(validTitle);
+
+            // Assert
+            result.IsSuccess.Should().BeTrue();
+            result.Value.Value.Should().Be(validTitle);
+        }
+
+        [Fact]
+        public void Create_Should_Succeed_When_Title_With_Numbers_And_Symbols()
+        {
+            // Arrange
+            var titleWithSymbols = "Electronics & Gadgets";
+
+            // Act
+            var result = CategoryTitle.Create(titleWithSymbols);
+
+            // Assert
+            result.IsSuccess.Should().BeTrue();
+            result.Value.Value.Should().Be(titleWithSymbols);
+        }
+
+        [Fact]
+        public void Create_Should_Succeed_When_Title_With_Unicode_Characters()
+        {
+            // Arrange
+            var unicodeTitle = "Электроника";
+
+            // Act
+            var result = CategoryTitle.Create(unicodeTitle);
+
+            // Assert
+            result.IsSuccess.Should().BeTrue();
+            result.Value.Value.Should().Be(unicodeTitle);
+        }
+
+        [Fact]
+        public void Create_Should_Succeed_With_Common_Category_Names()
+        {
+            var commonCategories = new[]
+            {
+                "Electronics",
+                "Clothing",
+                "Books",
+                "Home",
+                "Sports",
+                "Toys",
+                "Automotive",
+                "Real Estate",
+                "Jobs",
+                "Services"
+            };
+
+            foreach (var category in commonCategories)
+            {
+                // Act
+                var result = CategoryTitle.Create(category);
+
+                // Assert
+                result.IsSuccess.Should().BeTrue($"Category '{category}' should be valid");
+                result.Value.Value.Should().Be(category);
+            }
+        }
+    }
+}
